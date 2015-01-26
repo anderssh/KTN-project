916d58005ecdf573c2b14f972a15e363820b26ef
Anders Stiksrud Helmen <anders.s.helmen@gmail.com>
2015-01-26 08:29:06 +0100

    Cleaned the code a bit, still unfunctional code

--- Labbovinger/Oving_1/WebServer_to_file.py
+++ Labbovinger/Oving_1/WebServer_to_file.py
@@ -2,11 +2,11 @@
 #import socket module
 from socket import *
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
 #Prepare a sever socket
-#Fill in start
 serverSocket.bind(('',1291))
 serverSocket.listen(5)
-#Fill in end
+
 while True:
 	#Establish the connection
 	print 'Ready to serve...'
@@ -14,28 +14,23 @@ while True:
 	print 'Got connection from', addr[0], addr[1];
 	try:
 		message = connectionSocket.recv(1024)
-		#ripnt message
+		#print message
 		print message.split()[1]
 		f = open(message.split()[1])
 		outputdata = f.read()
+
 		#Send one HTTP header line into socket
-		#Fill in start
 		connectionSocket.send('HTTP/1.0 200 OK\n')
-		#connectionSocket.send('Content-Type: text/html\n')
-		#Fill in end
+		connectionSocket.send('Content-Type: text/html\n')
 		#Send the content of the requested file to the client
 		for i in range(0, len(outputdata)):
 			connectionSocket.send(outputdata[i])
 			connectionSocket.close()
 	except IOError:
 		#Send response message for file not found
-		#Fill in start
 		print 'IOerror'
 		#connectionSocket.send('HTTP/1.1 404 Not Found\n\n')
-		#Fill in end
 		#Close client socket
-		#Fill in start
 		connectionSocket.close()
-		#Fill in end
 	serverSocket.close()
 
