46ddada3a802dba50dc6cdaf412ef83f5b10d21b
Anders Stiksrud Helmen <anders.s.helmen@gmail.com>
2015-01-28 08:51:47 +0100

    Fixed all the bugs, code working as it should

--- Labbovinger/Oving_1/WebServer_without_file.py
+++ Labbovinger/Oving_1/WebServer_without_file.py
@@ -4,7 +4,7 @@ from socket import *
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
-serverSocket.bind(('',1291))
+serverSocket.bind(('',1288))
 serverSocket.listen(5)
 
 #Fill in end
@@ -14,14 +14,12 @@ while True:
 	(connectionSocket, addr) = serverSocket.accept()
 	print 'Got connection from', addr[0], addr[1];
 	try:
-		message = connectionSocket.recv(1024)
-		#ripnt message
-		print message.split()[1]
+		connectionSocket.recv(1024)
+
 		#Send one HTTP header line into socket
-		#Fill in start
-		connectionSocket.send('HTTP/1.0 200 OK\n')
-		connectionSocket.send('Content-Type: text/html\n')
-		connectionSocket.send('\n')
+		connectionSocket.send('HTTP/1.0 200 OK\r\n')
+		connectionSocket.send('Content-Type: text/html\r\n')
+		connectionSocket.send('\r\n')
 		connectionSocket.send("""
 			<html>
 			<body>
@@ -31,15 +29,17 @@ while True:
 			""")
 	except IOError:
 		#Send response message for file not found
-		#Fill in start
 		print 'IOerror'
 		connectionSocket.send('HTTP/1.1 404 Not Found\n\n')
-		#Fill in end
+
 		#Close client socket
-		#Fill in start
 		connectionSocket.close()
-		#Fill in end
-	finally:
+	except KeyboardInterrupt:
+		print "asdfasdfsdfasdf"
 		connectionSocket.close()
 		serverSocket.close()
+		raise
+
+		
+
 
